147b94e8a26cc8713ed97312c66f118e42497e87
R37RO <30890236+logneon@users.noreply.github.com>
2025-09-08 21:36:53 +0530

    Update fetch-youtube.js

--- fetch-youtube.js
+++ fetch-youtube.js
@@ -1,6 +1,12 @@
 const axios = require('axios');
 const fs = require('fs');
 
+// Ensure data directory exists
+if (!fs.existsSync('./data')) {
+    fs.mkdirSync('./data', { recursive: true });
+}
+
+
 const API_KEY = process.env.YOUTUBE_API_KEY;
 const CHANNEL_ID = process.env.CHANNEL_ID;
 const BASE_URL = 'https://www.googleapis.com/youtube/v3';
